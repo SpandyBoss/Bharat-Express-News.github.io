8e1dc90c16b5ed353e03e60884e59052ac62278b
SpandyBoss <rsehgal746@gmail.com>
2021-08-25 11:57:21 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log("This is my index js file");
+
+
+
+
+let newsAccordion = document.getElementById('newsAccordion');
+
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', `http://cors-anywhere.herokuapp.com/https://newsapi.org/v2/top-headlines?country=in&apiKey=be17a5a7e389464796681d8ec2bdfc94`, true);
+
+xhr.onload = function() {
+    if (this.status === 200) {
+        let json = JSON.parse(this.responseText);
+        let articles = json.articles;
+        console.log(articles);
+        let newsHtml = "";
+        articles.forEach(function(element, index) {
+            // console.log(element, index)
+            let news = `<div class="card">
+                            <div class="card-header" id="heading${index}">
+                                <h2 class="mb-0">
+                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
+                                    aria-expanded="false" aria-controls="collapse${index}">
+                                   <b>Breaking News ${index+1}:</b> ${element["title"]}
+                                </button>
+                                </h2>
+                            </div>
+
+                            <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}" data-parent="#newsAccordion">
+                                <div class="card-body"> ${element["content"]}. <a href="${element['url']}" target="_blank" >Read more here</a>  </div>
+                            </div>
+                        </div>`;
+            newsHtml += news;
+        });
+        newsAccordion.innerHTML = newsHtml;
+    } else {
+        console.log("Some error occured")
+    }
+}
+
+xhr.send()
