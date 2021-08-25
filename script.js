3aa7404d7ce03edfeea665cbde8f6bb10c927422
SpandyBoss <77114710+SpandyBoss@users.noreply.github.com>
2021-08-25 12:41:17 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let newsAccordion = document.getElementById('newsAccordion');
 
 
 const xhr = new XMLHttpRequest();
-xhr.open('GET', `http://cors-anywhere.herokuapp.com/http://newsapi.org/v2/top-headlines?country=in&apiKey=be17a5a7e389464796681d8ec2bdfc94`, true);
+xhr.open('GET', `https://newsapi.org/v2/top-headlines?country=in&apiKey=be17a5a7e389464796681d8ec2bdfc94`, true);
 
 xhr.onload = function() {
     if (this.status === 200) {
